ebdcf69efc11d74e7e1c056fe0a74f7a14285992
Conrado <conradocjo@gmail.com>
2020-05-11 20:31:24 -0300

    Inclusao do serviço no contexto do modulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from '../app/app-routing/app-routing.module'
-import { AppComponent } from './app.component';
-import { TopoComponent } from './topo/topo.component';
-import { HomeComponent } from './home/home.component';
-import { RodapeComponent } from './rodape/rodape.component';
-import { HttpModule } from '@angular/http';
-import { DiversaoComponent } from './diversao/diversao.component';
-import { RestaurantesComponent } from './restaurantes/restaurantes.component';
-import { OfertasService } from './services/ofertas.service';
-import { OfertaComponent } from './oferta/oferta.component';
-import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
-import { ComoUsarComponent } from './oferta/como-usar/como-usar.component';
-
-import { NgModule, LOCALE_ID } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import ptBr from '@angular/common/locales/pt';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from '../app/app-routing/app-routing.module';
+import { AppComponent } from './app.component';
+import { DiversaoComponent } from './diversao/diversao.component';
+import { HomeComponent } from './home/home.component';
+import { ComoUsarComponent } from './oferta/como-usar/como-usar.component';
+import { OfertaComponent } from './oferta/oferta.component';
+import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
+import { OrdemCompraSucessoComponent } from './ordem-compra-sucesso/ordem-compra-sucesso.component';
+import { OrdemCompraComponent } from './ordem-compra/ordem-compra.component';
+import { RestaurantesComponent } from './restaurantes/restaurantes.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { CarrinhoService } from './services/carrinho.service';
+import { OfertasService } from './services/ofertas.service';
+//Pipe
+import { DescricaoReduzidaPipe } from './shared/descricao-reduzida-pipe';
+import { TopoComponent } from './topo/topo.component';
 
-import {ReactiveFormsModule} from '@angular/forms'
+
 
 registerLocaleData(ptBr)
 
-//Pipe
-import { DescricaoReduzidaPipe } from './shared/descricao-reduzida-pipe';
-import { OrdemCompraComponent } from './ordem-compra/ordem-compra.component';
-import { OrdemCompraSucessoComponent } from './ordem-compra-sucesso/ordem-compra-sucesso.component';
 
 @NgModule({
   declarations: [
@@ -39,7 +40,7 @@ import { OrdemCompraSucessoComponent } from './ordem-compra-sucesso/ordem-compra
     DescricaoReduzidaPipe,
     OrdemCompraComponent,
     OrdemCompraSucessoComponent
-    
+
   ],
   imports: [
     BrowserModule,
@@ -47,7 +48,10 @@ import { OrdemCompraSucessoComponent } from './ordem-compra-sucesso/ordem-compra
     ReactiveFormsModule,
     AppRoutingModule
   ],
-  providers: [OfertasService, { provide: LOCALE_ID, useValue: 'pt' }],
+  providers: [
+    OfertasService, /* A declaração no nível  modulo terá o serviço compartilhado em toda aplicação. Posso declarar o provider das duas formas */
+    { provide: 'CarrinhoService', useValue: 'CarrinhoService' }, //o use Value é a referencia do Carrinho.
+    { provide: LOCALE_ID, useValue: 'pt' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
